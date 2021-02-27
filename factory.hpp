2cbdeacc2a2d6aad7fc40836de53a53fc1024aba
Koogleblitz <richardtobing@outlook.com>
2021-02-26 18:25:00 -0800

    factory takes in the arguments and creates a composite Base* object that is that results from the expression input

--- factory.hpp
+++ factory.hpp
@@ -1,5 +1,5 @@
-#ifndef FACTORY_HPP
-#define FACTORY_HPP
+#ifndef __FACTORY_HPP__
+#define __FACTORY_HPP__
 
 #include <iostream>
 #include <stdio.h>
@@ -10,47 +10,68 @@
 #include <ctype.h>
 
 //You have to include these classes/files form lab 3:
-//// #include "base.hpp"
-//// #include "Div.hpp"
-//// #include "Pow.hpp"
-//// #include "Sub.hpp"
-//// #include "add.hpp"
-//// #include "mult.hpp"
-//// #include "op.hpp"
-//// using namespace std;
+// #include "base.hpp"
+// #include "Div.hpp"
+// #include "Pow.hpp"
+// #include "Sub.hpp"
+// #include "add.hpp"
+// #include "mult.hpp"
+// #include "op.hpp"
+// using namespace std;
 
-struct Factory{
-	Factory(){};
-	Base* parse(char** strInput, int length){
-	  string strArr[length];
-    	  Base* composite;
 
-          for(int i = 0; i< length; i++){
-            strArr[i]= static_cast<string>(strInput[i]); 
-          }
+struct Factory {
+  Factory(){ }; 
+  Base* parse(char** strInput, int length){
+    string strArr[length];
+    Base* composite;
 
-	  if( strArr[1] == "+" ){
-      	    composite = new Add(new Op(stod(strArr[0])), new Op(		stod(strArr[2])));
-    	  }
-          else if( strArr[1] == "-" ){
-            composite = new Sub(new Op(stod(strArr[0])), new Op(stod            (strArr[2])));
-          }
-          else if( strArr[1] == "*" ){
-            composite = new Mult(new Op(stod(strArr[0])), new Op(sto            d(strArr[2])));
-          }
-          else if( strArr[1] == "/" ){
-            composite = new Div(new Op(stod(strArr[0])), new Op(stod            (strArr[2])));
-          }
-          else if( strArr[1] == "**" ){
-            composite = new Pow(new Op(stod(strArr[0])), new Op(stod            (strArr[2])));
-          }
-          else{
-            cout<< "invalid input"<<endl;
-          }
-	
+    for(int i = 0; i< length; i++){
+      strArr[i]= static_cast<string>(strInput[i]); 
+    }
 
-	  return composite;
-	}
 
+    if( strArr[1] == "+" ){
+      composite = new Add(new Op(stod(strArr[0])), new Op(stod(strArr[2])));
+    }
+    else if( strArr[1] == "-" ){
+      composite = new Sub(new Op(stod(strArr[0])), new Op(stod(strArr[2])));
+    }
+    else if( strArr[1] == "*" ){
+      composite = new Mult(new Op(stod(strArr[0])), new Op(stod(strArr[2])));
+    }
+    else if( strArr[1] == "/" ){
+      composite = new Div(new Op(stod(strArr[0])), new Op(stod(strArr[2])));
+    }
+    else if( strArr[1] == "**" ){
+      composite = new Pow(new Op(stod(strArr[0])), new Op(stod(strArr[2])));
+    }
+    else{
+      cout<< "invalid input"<<endl;
+    }
+
+    
+    for(int i = 3; i< length; i++){
+      if( strArr[i] == "+" ){
+        composite = new Add(new Op(composite->evaluate()), new Op(stod(strArr[i+1])));
+      }
+      else if( strArr[i] == "-" ){
+        composite = new Sub(new Op(composite->evaluate()), new Op(stod(strArr[i+1])));
+      }
+      else if( strArr[i] == "*" ){
+        composite = new Mult(new Op(composite->evaluate()), new Op(stod(strArr[i+1])));
+      }
+      else if( strArr[i] == "/" ){
+        composite = new Div(new Op(composite->evaluate()), new Op(stod(strArr[i+1])));
+      }
+      else if( strArr[i] == "**" ){
+        composite = new Pow(new Op(composite->evaluate()), new Op(stod(strArr[i+1])));
+      }
+      else{
+        cout<<"invalid input"<<endl;
+      }
+    }
+    return composite;
+  }
 };
 #endif
