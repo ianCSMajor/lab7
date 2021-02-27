1ea7900489cf14db47142244eb35d16f742d5278
Koogleblitz <richardtobing@outlook.com>
2021-02-26 17:54:24 -0800

    added a basic main and a basic factory, still need to implement

---
+++ factory.hpp
@@ -0,0 +1,33 @@
+#ifndef FACTORY_HPP
+#define FACTORY_HPP
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <string>
+#include <array>
+#include <ctype.h>
+
+struct Factory{
+	Factory(){};
+	Base* parse(char** strInput, int length){
+
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+//you hae to include these classes from lab3
+//#include "Div.hpp"
+//#include "Pow.hpp"
+//#include "Sub.hpp
+//#include "add.hpp"
+//#include "base.hpp"
+//#include "mult.hpp"
+//#include "op.hpp"
+
+
+//test cases should be removed from the final version of the main function
+int main (int argv, char** argc){
+
+
+
+
+return 0;
+}
