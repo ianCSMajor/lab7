90c26435860efc4a2e6d999c56292564214eb184
Koogleblitz <richardtobing@outlook.com>
2021-02-26 18:36:09 -0800

    main and factory have been tested on local IDE and works, just need to implement some nullptr logic for invalid inputs

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 using namespace std;
 
+#include "factory.hpp"
 
-//you hae to include these classes from lab3
-//#include "Div.hpp"
-//#include "Pow.hpp"
-//#include "Sub.hpp
-//#include "add.hpp"
-//#include "base.hpp"
-//#include "mult.hpp"
-//#include "op.hpp"
+
+//you have to include these classes from lab3
+#include "Div.hpp"
+#include "Pow.hpp"
+#include "Sub.hpp"
+#include "add.hpp"
+#include "base.hpp"
+#include "mult.hpp"
+#include "op.hpp"
 
 
 //test cases should be removed from the final version of the main function
 int main (int argv, char** argc){
+
+//----------------------Test Case----------------------------- 
+  char* test_val[11]; 
+  test_val[0] = "5"; 
+  test_val[1] = "+"; 
+  test_val[2] = "6"; 
+  test_val[3] = "-"; 
+  test_val[4] = "7"; 
+  test_val[5] = "*";
+  test_val[6] = "3";
+  test_val[7] = "/";
+  test_val[8] = "4";
+  test_val[9] = "**";
+  test_val[10] = "3";
+//------------------------------------------------------------  
 
   Factory* f = new Factory();
   Base* result = f->parse(test_val, 11);
@@ -21,8 +38,11 @@ int main (int argv, char** argc){
   string reslutStr= result->stringify();
   double resultDbl= result->evaluate();
 
-  cout << "Expression: " << reslutStr << endl;
-  cout << "Result: " << resultDbl << endl;
+    cout << "Expression: " << reslutStr << endl;
+    cout << "Result: " << resultDbl << endl;
   
+
+
+
 return 0;
 }
