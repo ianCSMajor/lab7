f92126e92b47cb01f72565b33cd11bf7d638d7cc
Koogleblitz <richardtobing@outlook.com>
2021-03-02 21:47:24 -0800

    added more tests

--- richardsTests.cpp
+++ richardsTests.cpp
@@ -28,6 +28,67 @@ TEST(FactorySuite, addition) {
   EXPECT_EQ("5+6", fac->textify());
 }
 
+
+TEST(FactorySuite, subtraction) {   
+	char* test_val[4];   
+	test_val[0] = "./calculator";   
+	test_val[1] = "5";   
+	test_val[2] = "-";   
+	test_val[3] = "2";    
+	Factory* fac = new Factory();   
+	Base* result = fac->parse(test_val, 4);    
+	EXPECT_EQ("5.000000 - 2.000000", result->stringify());   
+	EXPECT_EQ(3, result->evaluate());   
+	EXPECT_EQ("5-2", fac->textify()); 
+}
+
+TEST(FactorySuite, multiplication) {
+  char* test_val[4];
+  test_val[0] = "./calculator"; 
+  test_val[1] = "5"; 
+  test_val[2] = "\*"; 
+  test_val[3] = "6"; 
+
+  Factory* fac = new Factory();
+  Base* result = fac->parse(test_val, 4);
+
+  EXPECT_EQ("5.000000 * 6.000000", result->stringify());
+  EXPECT_EQ(30, result->evaluate());
+  EXPECT_EQ("5*6", fac->textify());
+}
+
+TEST(FactorySuite, division) {
+  char* test_val[4];
+  test_val[0] = "./calculator"; 
+  test_val[1] = "8"; 
+  test_val[2] = "/"; 
+  test_val[3] = "4"; 
+
+  Factory* fac = new Factory();
+  Base* result = fac->parse(test_val, 4);
+
+  EXPECT_EQ("4.000000 / 4.000000", result->stringify());
+  EXPECT_EQ(2, result->evaluate());
+  EXPECT_EQ("8/4", fac->textify());
+}
+
+
+TEST(FactorySuite, power) {
+  char* test_val[4];
+  test_val[0] = "./calculator"; 
+  test_val[1] = "2"; 
+  test_val[2] = "\*\*"; 
+  test_val[3] = "3"; 
+
+  Factory* fac = new Factory();
+  Base* result = fac->parse(test_val, 4);
+
+  EXPECT_EQ("2.000000 ** 3.000000", result->stringify());
+  EXPECT_EQ(8, result->evaluate());
+  EXPECT_EQ("2**3", fac->textify());
+}
+
+
 TEST(FactorySuite, InvalidInput) { 	
 char* test_val[3]; 	
 test_val[0] = "./calculator"; 	
